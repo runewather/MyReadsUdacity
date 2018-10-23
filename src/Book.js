699afe6d546d8f742f8776f8a9a2d61f9913b74e
Victo Principe <runewather@runewather.intrajus.tjrn>
2018-10-23 09:46:37 -0300

    init

---
+++ src/Book.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './App.css'
+
+class Book extends React.Component {
+    render() {
+        return (
+            <div className="book">
+                <div className="book-top">
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: "url('" + this.props.info.imageLinks.thumbnail + "');" }}></div>
+                <div className="book-shelf-changer">
+                    <select>
+                    <option value="move" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                    </select>
+                </div>
+                </div>
+                <div className="book-title">{this.props.info.title}</div>
+                <div className="book-authors">{this.props.info.authors[0]}</div>
+            </div>
+        )
+    }
+}
+
+Book.propTypes = {
+    info : PropTypes.object.isRequired
+}
+
+export default Book
---
+++ src/ListType.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class ListType extends React.Component {
+    
+}
+
+ListType.propTypes = {
+    name : PropTypes.string.isRequired,
+    objects : PropTypes.array.isRequired
+}
+
+export default ListType;
